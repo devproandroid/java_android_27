7d849db3a2aaa3430c43f5e2e8524baf5ad67afc
manht <manht@192.168.88.4>
2018-05-11 21:20:05 +0700

    Initialize project

---
+++ Lecture004Eclair/src/com/devpro/training/Eclair.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.devpro.training;
+
+/**
+ * @author manht
+ *
+ */
+public class Eclair {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
